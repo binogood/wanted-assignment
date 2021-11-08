7fca3d43b9167743f4d09b5e8b2e6040686620a6
binogood <binogood68@gmail.com>
2021-11-08 19:59:12 +0900

    Add: Fastapi main 및 run파일 생성 - fastapi main.py파일 생성 - fastapp 실행파일 run.py 생

---
+++ project/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.view import company_view, tag_view
+
+
+def create_app():
+    app = FastAPI()
+
+    app.include_router(company_view.router, prefix="/api")
+    # app.include_router(tag_view.router, prefix="tag")
+    @app.get("/")
+    def hello_wanted():
+        return "hello_wanted"
+
+    return app
+
+
---
+++ project/app/run.py
@@ -0,0 +1,7 @@
+from main import create_app
+import uvicorn
+
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run("run:app", host="0.0.0.0", port=8000, reload=True)
