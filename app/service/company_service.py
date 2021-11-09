02b4ad9ce7917f3d755df402298ee56d63fe2dd3
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2021-11-10 03:08:10 +0900

    fix : 함수 이름 변경

--- app/service/company_service.py
+++ app/service/company_service.py
@@ -43,7 +43,7 @@ def company_search_service(company_name, x_wanted_language, session):
 
 
 async def company_automatch_service(query, lang, session):
-    datas = company_dao.search_company_language_dao(query, lang, session)
+    datas = company_dao.autocomplete_company_language_dao(query, lang, session)
     company_search_list = []
     for data in datas:
         company_search_list.append({"company_name": data})
