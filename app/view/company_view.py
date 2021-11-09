437f8a7d0222dad33c3418dd27634f20645d1ff7
kim-hoontae <78228444+kim-hoontae@users.noreply.github.com>
2021-11-10 05:09:50 +0900

    Update company_view.py

--- app/view/company_view.py
+++ app/view/company_view.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Header
+from fastapi import APIRouter, Depends, Header, Request
 from sqlalchemy.orm import Session
 from typing import Optional
 from fastapi.responses import JSONResponse
@@ -12,7 +12,7 @@ from app.service import company_service
 router = APIRouter()
 
 
-@router.post("/companies")
+@router.post("/companies", tags=['company'])
 async def create_company(company_info: Company, x_wanted_language: Optional[str] = Header('ko'),
                          session: Session = Depends(db.session)):
     if not company_info.company_name:
@@ -26,7 +26,7 @@ async def create_company(company_info: Company, x_wanted_language: Optional[str]
     return JSONResponse(status_code=404, content='실패')
 
 
-@router.get("/companies/{company_name}")
+@router.get("/companies/{company_name}", tags=['company'])
 async def list_company(x_wanted_language: Optional[str] = Header('ko'), company_name: Optional[str] = None,
                        session: Session = Depends(db.session)):
     data = company_service.company_search_service(company_name, x_wanted_language, session)
@@ -34,7 +34,7 @@ async def list_company(x_wanted_language: Optional[str] = Header('ko'), company_
         return data
     return JSONResponse(status_code=404, content='실패')
   
-@router.get("/search")
+@router.get("/search", tags=['company'])
 async def name_autocomplete_company(query : Optional[str], request:Request, session: Session = Depends(db.session)):
     lang = request.headers.get("x-wanted-language")
     data = await company_service.company_automatch_service(query, lang, session)
