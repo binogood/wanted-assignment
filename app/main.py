d9ff5afae7eac82d0a244379f0d6b9c43e6eb8fb
kim-hoontae <78228444+kim-hoontae@users.noreply.github.com>
2021-11-10 04:43:02 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,22 +2,26 @@ from fastapi import FastAPI
 from dataclasses import asdict
 
 from view import company_view
-from app.config import conf
-from app.database.conn import db
+from config import conf
+from database.conn import db
 
+description = """
+# [Assignment 3] 원티드랩 과제
+
+
+### 팀원 : 강대훈, 김훈태, 안다민, 이무현, 송빈호, 정성헌
+
+"""
 
 def create_app():
-    app = FastAPI()
+    app = FastAPI(title="Wantedlab Restfull API",
+                  description=description)
     c = conf()
     conf_dict = asdict(c)
     db.init_app(app, **conf_dict)
 
     app.include_router(company_view.router)
-    # app.include_router(tag_view.router, prefix="tag")
-
-    @app.get("/")
-    def hello_wanted():
-        return "hello_wanted"
+    #app.include_router(tag_view.router, prefix="tag")
 
     return app
 
